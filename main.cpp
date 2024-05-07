25bcddeb6a4d363cf7f6d2fbe3f9d35b1fca058c
huynqbg <nqhuysmbg@gmail.com>
2024-05-07 18:33:43 +0700

    initial main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,34 @@
 
 using namespace std;
 
+class Book {
+public:
+    int id;
+    string title;
+    string author;
+    int quantity;
+};
+
+class Ebook: private Book {
+private:
+    string fileFormat
+    int fileSize
+};
+
+class Person {
+private:
+    int id;
+    string name;
+    string email;
+    string sex;
+    string birthday;
+    string address;
+    string phoneNumber;
+    string role;
+};
+
 int main() {
     cout << "Hello World";
+
     return 0;
 }
