423dea3e74705fbb74c8c90590450db4fe55fc7c
TheThinh <thinhthenguyen2004@gmail.com>
2024-05-07 16:21:10 +0700

    test pull data from dev-1

--- main.cpp
+++ main.cpp
@@ -2,7 +2,41 @@
 
 using namespace std;
 
+class Person{
+	private:
+		int id;
+		string 	name, email, sex, birthday, 
+				address, phoneNumber, role;
+		
+	public:
+		friend istream& operator >> (istream &in, Person &a);
+		friend ostream& operator << (ostream &out, Person a);
+};
+
+istream& operator >> (istream &in, Person &a)
+{
+	in >> a.id;
+	scanf("\n");
+	getline(in, a.name);
+	in >> a.role;
+	return in;
+}
+
+ostream& operator << (ostream &out, Person a)
+{
+	out << a.id << " " << a.name << " " << a.role << endl;
+	return out;
+}
+
+class User{
+	private:
+		int id;
+		string email, password;
+};
+
 int main() {
-    cout << "Hello World";
+    Person a;
+	cin >> a;
+	cout << a;
     return 0;
 }
