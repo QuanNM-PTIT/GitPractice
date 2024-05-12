a5c749875d99705feff431e3c09599ef541c7244
Yen Nguyen <yen1422mh@gmail.com>
2024-05-13 00:01:57 +0700

    Sua mot so loi trong class Person va BorrowInfo

--- main.cpp
+++ main.cpp
@@ -1,36 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
-int IdPerson = 0, borrowInfoId = 0;
 class Person;
-void CapNhatIdPerson(){
-	ifstream input("F:\\Project\\GitPractice\\people.txt");
-	vector<string> v;
+int UpdateIdPerson(){ // cap nhat ID person
+	ifstream input("people.txt");
+	set<int> se;
 	if(input.is_open()){
 		string s;
 		while(getline(input, s)){
-			v.push_back(s);
+			int i = 0;
+			string tmp = "";
+			while(s[i] != ']'){
+				if(isdigit(s[i])) tmp += s[i];
+				i++;
+			}
+			se.insert(stoi(tmp));
 		}
-		IdPerson = v.size();
 		input.close();
 	}
-	else{
-		cout << "Khong mo duoc file\n" ;
+	else cout << "Khong mo duoc file!" << endl;
+	int NextId = 1;
+	while(se.count(NextId)){
+		NextId++;
 	}
+	return NextId;
 }
-void CapNhatBorrowInfoId(){
-	ifstream input("F:\\Project\\GitPractice\\borrowInfos.txt");
-	vector<string> v;
+int UpdateIdBorrowInfo(){ // cap nhat ID muon sach
+	ifstream input("borrowInfos.txt");
+	set<int> se;
 	if(input.is_open()){
 		string s;
 		while(getline(input, s)){
-			v.push_back(s);
+			int i = 0;
+			string tmp = "";
+			while(s[i] != ']'){
+				if(isdigit(s[i])) tmp += s[i];
+				i++;
+			}
+			se.insert(stoi(tmp));
 		}
-		borrowInfoId = v.size();
 		input.close();
 	}
-	else{
-		cout << "Khong mo duoc file\n" ;
+	else cout << "Khong mo duoc file!" << endl;
+	
+	int NextId = 1;
+	while(se.count(NextId)){
+		NextId++;
 	}
+	return NextId;
 }
 class Person{
 private:
@@ -38,14 +54,10 @@ private:
     string name = "", email = "", sex = "", birthdate = "", phoneNumber = "", role = "", address = "";
 public:
 	Person(){
-		CapNhatIdPerson();
-		IdPerson++;
-		this->id = IdPerson;
+		this->id = UpdateIdPerson();
 	}
     Person(string name, string email, string sex, string birthdate, string address, string phoneNumber, string role){
-    	CapNhatIdPerson();
-        IdPerson++;
-        this->id = IdPerson;
+        this->id = UpdateIdPerson();
         this->name = name;
         this->email = email;
         this->sex = sex;
@@ -54,7 +66,7 @@ public:
         this->phoneNumber = phoneNumber;
     }
     void In(Person x){
-        cout << x.name << ' ' << x.email << ' ' << x.sex << endl;
+        cout << x.id << ' ' << x.name << ' ' << x.email << ' ' << x.sex << endl;
     }
 
 };
@@ -63,27 +75,23 @@ private:
     int id, personId = 0, bookId = 0, ebookId = 0;
 public:
 	BorrowInfo() {
-        CapNhatBorrowInfoId();
-        borrowInfoId++;
-        this->id = borrowInfoId;
+        this->id = UpdateIdBorrowInfo();
     }
     BorrowInfo(int personId, int bookId, int ebookId ){
-    	CapNhatBorrowInfoId();
-        borrowInfoId++;
-        this->id = borrowInfoId;
+        this->id = UpdateIdBorrowInfo();
         this->personId = personId;
         this->bookId = bookId;
         this->ebookId = ebookId;
     }
-    void addInfo(){
+    void addInfo(){ // them thong tin muon sach 
     	int Id1, Id2, Id3;
-    	cout << "Nhap personId: ";
+    	cout << "Nhap ID nguoi muon: ";
     	cin >> Id1;
    		cout << "Nhap ID sach: ";
    		cin >> Id2;
    		cout << "Nhap ID Ebook: ";
    		cin >> Id3;
-        ofstream output("F:\\Project\\GitPractice\\borrowInfos.txt", ios::app);
+        ofstream output("borrowInfos.txt", ios::app);
 		if (output.is_open()){
 			output << endl;
 			output << '[' << id  << ']'  << ' '  << '[' << Id1 << ']' << ' '  << '[' << Id2 << ']' << ' '  << '[' << Id3 << ']';
@@ -93,8 +101,88 @@ public:
         	cout << "Khong mo duoc file\n";
 		}
     }
-//	void updateInfor() 
+	void updateInfo(){
+		BorrowInfo update;
+		cout << "__Cap nhat thong tin muon sach__\n";
+		cout << "Nhap ID muon sach: ";
+		cin >> update.id;
+		cout << "\nNhap ID nguoi muon: ";
+		cin >> update.personId;
+		cout << "\nNhap ID sach: ";
+		cin >> update.bookId;
+		cout << "\nNhap ID Ebook: ";
+		cin >> update.ebookId;
+		// Doc du lieu tu file BorrowInfos.txt
+		ifstream input("borrowInfos.txt");
+		// doc du lieu tu file borrowInfos.txt 
+		vector<BorrowInfo> info;
+		if(input.is_open()){
+			string s; 
+			while(getline(input, s)){
+				BorrowInfo oldInfo;
+				string tmp;
+				stringstream ss(s);
+				int cnt = 0;
+				while(ss >> tmp){
+					if(!tmp.empty()){
+						// xoa dau []
+						cnt++;
+						tmp.erase(0, 1);
+						tmp.erase(tmp.size() - 1, 1);
+						if(cnt == 1){
+							oldInfo.id = stoi(tmp);
+						} 
+						else if(cnt == 2) oldInfo.personId = stoi(tmp);
+						else if(cnt == 3) oldInfo.bookId = stoi(tmp);
+						else if(cnt == 4){
+							oldInfo.ebookId = stoi(tmp);
+							info.push_back(oldInfo);
+						}
+					}
+				}
+			}
+			input.close();
+		}
+		else cout << "Khong mo duoc file!\n";
+		// ghi lai vao file
+		ofstream output("borrowInfos.txt");
+		if(output.is_open()){
+			bool check = true;
+			for(auto borrowRecord: info){
+				if(borrowRecord.id == update.id){
+					borrowRecord.personId = update.personId;
+					borrowRecord.bookId = update.bookId;
+					borrowRecord.ebookId = update.ebookId;
+					check = false;
+					cout << "Cap nhat thong tin thanh cong!\n";
+				}
+				output << "[" << borrowRecord.id << "] [" << borrowRecord.personId << "] [" << borrowRecord.bookId << "] [" << borrowRecord.ebookId << "]" << endl;
+			}
+			if(check) cout << "Khong tim thay thong tin muon cap nhat!\n"; 
+			output.close();
+		}
+		else cout << "Khong mo duoc file!\n";
+	} 
 };
+void DisplayMenu(){
+	cout << "--------------------MENU--------------------" << endl;
+	cout << "2. Dang ky." << endl;
+	cout << "4. Sua thong tin sach." << endl;
+	cout << "6. Muon sach." << endl;
+	cout << "8. Lay thong tin cac quyen sach." << endl;
+	cout << "10. Lay thong tin tat ca cac Ebook hien co." << endl;
+	cout << "12. Hien thi thong tin tat ca cac quyen sach da muon." << endl;
+	cout << "14. Chinh sua thong tin ca nhan" << endl;
+	cout << "16. Dang xuat." << endl;
+	cout << "---------------------------------------------" << endl;
+	
+
+}
 int main() {
+	// Person a("Tui", "Tui@gmail.com", "Nu", "14/02/2004", "Van Quan - Ha Dong", "0122345", "Sv");
+	// a.In(a);
+//	DisplayMenu();
+	BorrowInfo a;
+	a.updateInfo();
 	return 0;
 }
