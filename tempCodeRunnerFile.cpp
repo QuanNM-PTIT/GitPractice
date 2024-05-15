757d90d54a837b7c029538cbd35245b83fb4372e
PhuongDaiThang <ThangPD.B22CN816@stu.ptit.edu.vn>
2024-05-16 00:12:37 +0700

    fix login, create c,e,d

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,34 +1,45 @@
-void Login() {
+void Login()
+{
     string userName, password;
     cout << "Nhap Email: \n";
     cin >> userName;
     cout << "Nhap password: \n";
     cin >> password;
     ifstream filein("users.txt");
-    if (!filein.is_open()) {
+    if (!filein.is_open())
+    {
         cout << "Khong the mo tep users.txt\n";
     }
-    else {
+    else
+    {
         string tmp;
-        while (getline(filein, tmp)) {
-            if (tmp.find(userName) != string::npos && tmp.find(password) != string::npos) {
+        while (getline(filein, tmp))
+        {
+            if (tmp.find(userName) != string::npos && tmp.find(password) != string::npos)
+            {
                 cout << "Dang nhap thanh cong\n";
-                filein.close(); // Đóng file users.txt trước khi tiếp tục
+                loggedIn = true; // Đánh dấu đã đăng nhập thành công
+                filein.close();  // Đóng file users.txt trước khi tiếp tục
                 ifstream peopleFile("people.txt");
-                if (!peopleFile.is_open()) {
+                if (!peopleFile.is_open())
+                {
                     cout << "Khong the mo tep people.txt\n";
-                } else {
+                }
+                else
+                {
                     string name;
-                    while (getline(peopleFile, tmp)) {
-                        if (tmp.find(userName) != string::npos) {
+                    while (getline(peopleFile, tmp))
+                    {
+                        if (tmp.find(userName) != string::npos)
+                        {
                             // Tìm thấy email trong file people.txt
                             // Sử dụng stringstream để lấy tên từ dòng chứa email
                             stringstream ss(tmp);
                             string bracket;
-                            ss >> bracket; // Bỏ qua ký tự '['
+                            ss >> bracket;          // Bỏ qua ký tự '['
                             getline(ss, name, ']'); // Lấy tên đến khi gặp ký tự ']'
-                            // Loại bỏ dấu '[' thừa ở đầu tên
-                            name = name.substr(1);
+
+                            name = name.substr(2); // Bỏ đi dấu [ ở đầu tên
                             cout << "Welcome " << name << "!\n";
                             break; // Kết thúc vòng lặp sau khi tìm thấy tên
                         }
