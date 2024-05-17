17011ba49fb2a4e1fe58c67b154fe390fdcc0e0d
Jimmy Nguyễn <cityuongbi07@gmail.com>
2024-05-17 10:07:17 +0700

    Revert "Revert "Minh dev""

--- main.cpp
+++ main.cpp
@@ -493,6 +493,14 @@ public:
         this->title = title;
     }
 
+    string getAuthor() const{
+        return this -> author;
+    }
+    
+    void setAuthor(const string& author) {
+        this -> author = author;
+    }
+
     int getQuantity() const {
         return this->quantity;
     }
@@ -602,17 +610,7 @@ public:
         }
     }
 
-    void updateBook(const string& filename, const string& newBookInfo) {
-        // Tìm id từ thông tin mới của sách
-        size_t startPos = newBookInfo.find("[");
-        size_t endPos = newBookInfo.find("]", startPos);
-        if (startPos == string::npos || endPos == string::npos) {
-            cerr << "Khong the cap nhat thong tin sach vi khong tim thay ID." << endl;
-            return;
-        }
-
-        string newBookId = newBookInfo.substr(startPos + 1, endPos - startPos - 1);
-
+    void updateBook(const string& filename, const string& newBookInfo, string newBookID) {
         ifstream inFile(filename); 
         ofstream outFile("temp_books.txt"); 
 
@@ -626,11 +624,11 @@ public:
             
             // Nếu như vẫn còn đọc được dữ liệu
             if (startPos != string::npos && endPos != string::npos) {
-                string bookId = line.substr(startPos + 1, endPos - startPos - 1);
+                string bookID = line.substr(startPos + 1, endPos - startPos - 1);
 
                 // Nếu id của sách cần cập nhật == id hiện tại trong books.txt thì thay thế nó
-                if (bookId == newBookId) {
-                    outFile << newBookInfo << endl;
+                if (bookID == newBookID) {
+                    outFile << '[' << bookID << "] " << newBookInfo << endl;
                     found = true; // Đánh dấu là đã tìm thấy và cập nhật thông tin của cuốn sách
                 } 
                 // Nếu là các dữ liệu khác thì đẩy thẳng vào file
@@ -660,12 +658,12 @@ public:
                 return;
             }
 
-            cout << "Da cap nhat thong tin cua cuon sach có ID = " << newBookId << " thanh cong." << endl;
+            cout << "Da cap nhat thong tin cuon sach co ID = " << newBookID << " thanh cong." << endl;
         } 
         else {
             // Xoá file tạm thời nếu không tìm thấy cuốn sách cần cập nhật
             remove("temp_books.txt");
-            cout << "Khong tim thay cuon sach co ID = " << newBookId << " trong file." << endl;
+            cout << "Khong tim thay cuon sach co ID = " << newBookID << " trong file." << endl;
         }
     }
 
@@ -705,6 +703,40 @@ public:
             cout << endl;
         }
     }
+
+    Book getData(const string& filename, const string& targetID, int data) {
+        ifstream file(filename);
+        string line;
+        int cnt = 0;
+
+        while (getline(file, line)) {
+            ++cnt;
+        }
+
+        file.close();
+
+        vector<vector<string>> bookData(data);
+
+        for (int i = 0; i < data; ++i) {
+            bookData[i] = getInformationFromFile(filename, i);
+        }
+
+        int pos = -1;
+        for (int i = 0; i < cnt; ++i) {
+            if (bookData[0][i] == targetID) {
+                pos = i;
+                break;
+            }
+        }
+
+        if (pos == -1) {
+            cout << "Khong ton tai cuon sach co ID la: " << targetID << '!' << endl;
+        }
+        else {
+            return Book(bookData[1][pos], bookData[2][pos], stoi(bookData[3][pos]));
+        }
+    }
+
 };
 
 class EBook : public Book {
@@ -789,14 +821,16 @@ bool isExistAlphaOrNum(string& s) {
 }
 
 int cnt = 0;
+
 vector<string> attributeOfPerson = {"ten", "email", "gioi tinh", "ngay sinh", "dia chi", "so dien thoai"};
+vector<string> attributeOfBook = {"ten sach", "ten tac gia", "so luong"};
 
 string input(string& data, vector<string>& attribute) {
     cout << "Nhap vao " << attribute[cnt] << " ban muon chinh sua, neu khong chinh sua gi thi nhap (Khong): ";
     scanf("\n");
     getline(cin, data);
     while (isExistAlphaOrNum(data) == false) {
-        cout    << "Nhap sai du lieu cho, yeu cau phai co it nhat 1 ky tu hoac 1 so!" << endl
+        cout    << "Nhap sai du lieu cho, yeu cau phai co it nhat 1 ky tu!" << endl
                 << "Nhap lai  " << attribute[cnt] << " ban muon chinh sua, neu khong chinh sua gi thi nhap (Khong): ";
         scanf("\n");
         getline(cin, data);
@@ -820,6 +854,7 @@ string editPersonalData(Person& p) {
 
     string name;
     name = input(name, attributeOfPerson);
+    name = input(name, attributeOfPerson);
     if (name == "-1") {
         info += '[' + p.getName() + "] ";
     }
@@ -828,6 +863,7 @@ string editPersonalData(Person& p) {
     }
 
     string email;
+    email = input(email, attributeOfPerson);
     email = input(email, attributeOfPerson);
     if (email == "-1") {
         info += '[' + p.getEmail() + "] ";
@@ -838,6 +874,7 @@ string editPersonalData(Person& p) {
 
     string sex;
     sex = input(sex, attributeOfPerson);
+    sex = input(sex, attributeOfPerson);
     if (sex == "-1") {
         info += '[' + p.getSex() + "] ";
     }
@@ -846,6 +883,7 @@ string editPersonalData(Person& p) {
     }
 
     string birthdate;
+    birthdate = input(birthdate, attributeOfPerson);
     birthdate = input(birthdate, attributeOfPerson);
     if (birthdate == "-1") {
         info += '[' + p.getBirthdate() + "] ";
@@ -856,6 +894,7 @@ string editPersonalData(Person& p) {
 
     string address;
     address = input(address, attributeOfPerson);
+    address = input(address, attributeOfPerson);
     if (address == "-1") {
         info += '[' + p.getAddress() + "] ";
     }
@@ -865,12 +904,47 @@ string editPersonalData(Person& p) {
 
     string phoneNumber;
     phoneNumber = input(phoneNumber, attributeOfPerson);
+    phoneNumber = input(phoneNumber, attributeOfPerson);
     if (phoneNumber == "-1") {
         info += '[' + p.getPhoneNumber() + "] ";
     }
     else {
         info += '[' + phoneNumber + "] ";
     }
+    return info;
+}
+
+string editBookData(Book& b){
+    cnt = 0;
+    string info = "";
+
+    string title;
+    title = input(title, attributeOfBook);
+    if (title == "-1") {
+        info += '[' + b.getTitle() + "] ";
+    }
+    else {
+        info += '[' + title + "] ";
+    }
+
+    string author;
+    author = input(author, attributeOfBook);
+    if (title == "-1") {
+        info += '[' + b.getAuthor() + "] ";
+    }
+    else {
+        info += '[' + author + "] ";
+    }
+
+    string quantity;
+    quantity = input(quantity, attributeOfBook);
+    if (title == "-1") {
+        info += '[' + to_string(b.getQuantity()) + "] ";
+    }
+    else {
+        info += '[' + quantity + "] ";
+    }
+
     return info;
 }
 
@@ -977,12 +1051,26 @@ int main() {
                         cout << "Ban khong co quyen chinh sua thong tin sach!" << endl;
                     }
                     else {
-                        cout << "Nhap vao thong tin sach can chinh sua:" << endl;
+                        vector<string> idBook = getInformationFromFile("books.txt", 0);
+                        string id;
+                        cout << "Nhap vao id cuon sach ban muon sua: ";
+                        cin >> id;
+
+                        while (isValid(idBook, id) == true) {
+                            cout    << "Khong ton tai cuon sach co id: " << id << endl
+                                    << "Vui long nhap id cua cuon sach hop le !!!" << endl
+                                    << "Nhap lai id cua cuon sach ban muon sua: ";
+                            cin >> id;
+                        }
+
+                        Book tmp;
+                        tmp = b.getData("books.txt", id, 4);
+
                         string info;
-                        scanf("\n");
-                        getline(cin, info);
-                        b.updateBook("books.txt", info);
+                        info = editBookData(tmp);
+                        b.updateBook("books.txt", info, id);
                     }
+                    cout << endl;
                     break;
 
                 case 5:
